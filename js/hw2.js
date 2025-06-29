e336395faac50e43d5b05912cfb8cd5107085054
TKolchuk <tatyana.kolchuk@yandex.ru>
2025-06-29 20:38:43 +1000

    убрана горизонтальная полоса прокрутки в мобильной версии

--- js/hw2.js
+++ js/hw2.js
@@ -1,45 +1,45 @@
-// // Задание 1
-// let a;
-// a = 10;
-// alert(a);
-// a = 20;
-// alert(a);
+// Задание 1
+let a;
+a = 10;
+alert(a);
+a = 20;
+alert(a);
 
-// // Задание 2
-// let yearOfRelease = 2007;
-// alert(`Первый iPhone был выпущен в ${yearOfRelease}`)
+// Задание 2
+let yearOfRelease = 2007;
+alert(`Первый iPhone был выпущен в ${yearOfRelease}`)
 
-// // Задание 3
-// let creatorName = 'Брендан Эйх';
-// alert(`Создателем языка JavaScript является ${creatorName}`)
+// Задание 3
+let creatorName = 'Брендан Эйх';
+alert(`Создателем языка JavaScript является ${creatorName}`)
 
-// // Задание 4
-// let q = 10;
-// let w = 2; 
-// alert(`Сумма равна ${q + w}, разность ${q - w}, произведение ${q * w}, частное ${q / w}`)
+// Задание 4
+let q = 10;
+let w = 2; 
+alert(`Сумма равна ${q + w}, разность ${q - w}, произведение ${q * w}, частное ${q / w}`)
 
-// // Задание 5
-// let result = 2**5;
-// alert(result);
+// Задание 5
+let result = 2**5;
+alert(result);
 
-// // Задание 6
-// let s = 9;
-// let b = 2;
-// alert(s % b);
+// Задание 6
+let s = 9;
+let b = 2;
+alert(s % b);
 
-// // Задание 7
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num++; 
-// num--; 
-// alert(num);
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++; 
+num--; 
+alert(num);
 
-// // Задание 8
-// let age = prompt("Ваш возраст?");
-// alert(age);
+// Задание 8
+let age = prompt("Ваш возраст?");
+alert(age);
 
 // Задание 9
 let user = {
