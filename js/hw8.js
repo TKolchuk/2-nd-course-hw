c96c8f64eba87cbff7aaba57894b49d84cd89fa1
TKolchuk <tatyana.kolchuk@yandex.ru>
2025-10-20 10:55:59 +1000

    задания

---
+++ js/hw8.js
@@ -0,0 +1,77 @@
+// 1
+const people = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
+
+
+console.log(people.sort((a, b) => a.age - b.age));
+
+//2
+function isPositive(num) {
+    return num > 0;
+}
+function isMale(person) {
+    return person.gender === 'male';
+}
+function filter(arr, ruleFunction) {
+    const output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (ruleFunction(arr[i])) {
+         output.push(arr[i]);
+        }
+    }
+
+    return output;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people2 = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people2, isMale));
+
+//3
+let count = 0;
+
+const interval = setInterval(() => {
+    console.log(new Date().toString());
+    count += 3;
+
+    if (count >= 30) {
+        clearInterval(interval);
+        console.log('30 секунд прошло');
+    }
+}, 3 * 1000);
+
+//4
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+});
+
+
+//5
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
