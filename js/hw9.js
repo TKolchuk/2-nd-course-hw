f1c891a1997537bbbd8629ddcdfaf5ad01884be6
TKolchuk <tatyana.kolchuck@yandex.ru>
2026-04-16 12:19:26 +1000

    все задания

--- js/hw9.js
+++ js/hw9.js
@@ -24,3 +24,39 @@ const btnNewHeadingEl = document.querySelector(".btnNewHeading");
 btnNewHeadingEl.addEventListener('click', () => {
     newHeadingEl.textContent = 'Привет, мир!'
 });
+
+//4
+
+const descriptions = document.querySelectorAll('.description4');
+descriptions.forEach(element => {
+    element.textContent = 'Измененный текст';
+});
+
+
+//5
+
+const descriptEl = document.querySelectorAll('.description');
+
+descriptEl.forEach(element => {
+    element.textContent = 'Новый текст';
+});
+
+//6
+
+const btnAdd = document.getElementById('btnAdd');
+        
+btnAdd.addEventListener('click', () => {
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = 'Новый абзац';
+    document.body.appendChild(newParagraph);
+});
+
+//7
+
+const btnDel = document.getElementById('btnDel');
+
+btnDel.addEventListener('click', function() {
+    const targetElement = document.querySelector('.description7');
+    targetElement.remove();
+   
+});
