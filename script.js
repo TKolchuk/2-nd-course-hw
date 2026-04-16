f73ba801888d637236eeb9c179b7d2a7def19fbb
TKolchuk <tatyana.kolchuck@yandex.ru>
2026-04-16 13:46:35 +1000

    игра генератор цветов

--- script.js
+++ script.js
@@ -137,3 +137,15 @@ function RockPaperScissors() {
     );
   }
 }
+
+
+function RandomColorGenerator() {
+  const color = '#' + Math.floor(Math.random() * 16777215)
+                               .toString(16)
+                               .padStart(6, '0')
+                               .toUpperCase();
+
+  console.log (color);                                      
+  document.body.style.backgroundColor = color;
+        
+}
